269816f45fbfa0a907c74e438067395af7f2d6b6
mrohith29 <mariyalarohith29@gmail.com>
2023-12-13 17:19:43 +0530

    update

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function setDate() {
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60) * 6) + 90;
